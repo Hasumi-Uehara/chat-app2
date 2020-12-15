d3e18b5750694d4506ad1ce8e1ae1e011a4cef37
Hasumi-Uehara <slammygirl32@gmail.com>
2020-12-15 09:26:06 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,26 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    # 新規チャットを保存するために「空のレコード」と「ルームのid」を用意しなければならない。
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    # roomsテーブルからroom_idを取得。
+    @message = @room.messages.new(message_params)
+    # room_idに紐づいたメッセージのインスタンスを生成。
+    # message_paramsを引数にして、privateメソッドを呼び出す。
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
